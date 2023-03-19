ae849373dfeae61e70bea42ecc82c4682ce4173f
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-19 19:23:56 +0100

    Add storybook to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'company',
+  //   loadChildren: () => import('./pages/company/company.module').then(m => m.CompanyPageModule)
+  // },
+  // {
+  //   path: 'panel',
+  //   children: [
+  //     {
+  //       path: 'specialist',
+  //       loadChildren: () => import('./pages/panels/specialist/specialist.module').then(m => m.SpecialistPageModule)
+  //     },
+  //     {
+  //       path: '**',
+  //       redirectTo: '/',
+  //     },
+  //   ]
+  // },
+  // {
+  //   path: 'authorization',
+  //   canActivate: [AuthorizationGuard],
+  //   loadChildren: () => import('./pages/authorization/authorization.module').then(m => m.AuthorizationPageModule)
+  // },
+  // // Redirect
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'biz'
+  // },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundPageModule)
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: '/',
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
