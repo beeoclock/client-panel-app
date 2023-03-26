677a7d39ad552f9edde881a18e1cfc97aa4391be
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-03-26 10:43:25 +0200

    Delete not found page, now it is 404

--- src/domains/utility/presentation/index.ts
+++ src/domains/utility/presentation/index.ts
@@ -2,10 +2,6 @@ import {Route} from '@angular/router';
 
 export default [
   {
-    path: 'not-found',
-    loadComponent: () => import('./pages/not-found')
-  },
-  {
     path: '404',
     loadComponent: () => import('./pages/404')
   }
--- src/routers.ts
+++ src/routers.ts
@@ -63,6 +63,6 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '/not-found',
+    redirectTo: '/404',
   }
 ];
