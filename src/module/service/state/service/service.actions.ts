c62da15ded6085d455e9cfd55b1a3de40f7bfd0f
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-05-31 08:24:40 +0200

    Add one base namespace of actions for usual cases

--- src/module/service/state/service/service.actions.ts
+++ src/module/service/state/service/service.actions.ts
@@ -1,58 +1,32 @@
-import {IPagination_QueryParams} from "@utility/domain/pagination";
+import {BaseActions} from "@utility/state/base/base.actions";
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace ServiceActions {
 
-  export class GetList {
-    public static readonly type = '[Service API] Get List';
+  export class GetList extends BaseActions.GetItem {
+    public static override readonly type = '[Service API] Get List';
   }
 
-  export class DeleteItem {
-    public static readonly type = '[Service API] Delete Item';
-
-    constructor(
-      public payload: {
-        id: string;
-        refreshList?: boolean;
-        goToTheList?: boolean;
-      },
-    ) {
-    }
+  export class DeleteItem extends BaseActions.DeleteItem {
+    public static override readonly type = '[Service API] Delete Item';
   }
 
-  export class GetItem {
-    public static readonly type = '[Service API] Get Item';
-
-    constructor(
-      public payload: string,
-    ) {
-    }
+  export class GetItem extends BaseActions.GetItem {
+    public static override readonly type = '[Service API] Get Item';
   }
 
   // Updates of state
 
-  export class UpdateFilters {
-    public static readonly type = '[Service State] Update Filters';
-
-    constructor(
-      public payload: {
-        search: string | undefined;
-      },
-    ) {
-    }
+  export class UpdateFilters extends BaseActions.UpdateFilters {
+    public static override readonly type = '[Service State] Update Filters';
   }
 
-  export class UpdateQueryParamsAtNavigator {
-    public static readonly type = '[Service State] Update QueryParams At Navigator';
+  export class UpdateQueryParamsAtNavigator extends BaseActions.UpdateQueryParamsAtNavigator {
+    public static override readonly type = '[Service State] Update QueryParams At Navigator';
   }
 
-  export class UpdatePaginationFromQueryParams {
-    public static readonly type = '[Service State] Update Pagination from QueryParams';
-
-    constructor(
-      public payload: IPagination_QueryParams
-    ) {
-    }
+  export class UpdatePaginationFromQueryParams extends BaseActions.UpdatePaginationFromQueryParams {
+    public static override readonly type = '[Service State] Update Pagination from QueryParams';
   }
 
 }
---
+++ src/module/utility/state/base/base.actions.ts
@@ -0,0 +1,58 @@
+import {IPagination_QueryParams} from "@utility/domain/pagination";
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace BaseActions {
+
+  export abstract class GetList {
+    public static readonly type: string = '[TODO] Not Implemented Yet!';
+  }
+
+  export abstract class DeleteItem {
+    public static readonly type: string = '[TODO] Not Implemented Yet!';
+
+    constructor(
+      public payload: {
+        id: string;
+        refreshList?: boolean;
+        goToTheList?: boolean;
+      },
+    ) {
+    }
+  }
+
+  export abstract class GetItem {
+    public static readonly type: string = '[TODO] Not Implemented Yet!';
+
+    constructor(
+      public payload: string,
+    ) {
+    }
+  }
+
+  // Updates of state
+
+  export abstract class UpdateFilters {
+    public static readonly type: string = '[TODO] Not Implemented Yet!';
+
+    constructor(
+      public payload: {
+        search: string | undefined;
+      },
+    ) {
+    }
+  }
+
+  export abstract class UpdateQueryParamsAtNavigator {
+    public static readonly type: string = '[TODO] Not Implemented Yet!';
+  }
+
+  export abstract class UpdatePaginationFromQueryParams {
+    public static readonly type: string = '[TODO] Not Implemented Yet!';
+
+    constructor(
+      public payload: IPagination_QueryParams
+    ) {
+    }
+  }
+
+}
