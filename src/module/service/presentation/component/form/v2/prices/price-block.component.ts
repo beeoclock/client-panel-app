662b908e65554c3b3d2238daa425b3f42be38e87
Karba <ivan.karbashevskyi@gmail.com>
2023-08-02 09:08:32 +0200

    #146 | Done! (#151)

--- src/module/service/presentation/component/form/v2/prices/price-block.component.ts
+++ src/module/service/presentation/component/form/v2/prices/price-block.component.ts
@@ -19,12 +19,13 @@ import {PriceForm} from "@service/form/service.form";
         <input-badge-component
           mask="00:00"
           [control]="durationControl"
-          [placeholder]="'Format 00:00' | translate"
-          [label]="'Duration' | translate"
-          [badge]="'hours:minute' | translate">
+          placeholder="00:00"
+          [label]="'general.duration' | translate"
+          [badge]="'general.priceTimeFormatPrompt' | translate">
         </input-badge-component>
-        <price-and-currency-component [priceControl]="priceForm.controls.price"
-                                      [currencyControl]="priceForm.controls.currency"></price-and-currency-component>
+        <price-and-currency-component
+          [priceControl]="priceForm.controls.price"
+          [currencyControl]="priceForm.controls.currency"></price-and-currency-component>
       </div>
       <!--      <service-form-prices-specialists-component [label]="'Specialists' | translate"></service-form-prices-specialists-component>-->
     </div>
