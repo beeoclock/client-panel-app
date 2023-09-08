2155f473e51be6b9c18f5722daae6222a1ac3cd2
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-08 02:28:37 +0200

    Add new firebase beeoclock-develop context ot difenert modes

--- src/environment/environment.emulator.authorization.ts
+++ src/environment/environment.emulator.authorization.ts
@@ -35,14 +35,14 @@ export const environment = {
     }
   },
   firebase: {
-    options: {
-      apiKey: "AIzaSyDwj-I3xgErbKnJqMVGqd1uqIb20qRlS44",
-      authDomain: "bee-o-clock.firebaseapp.com",
-      projectId: "bee-o-clock",
-      storageBucket: "bee-o-clock.appspot.com",
-      messagingSenderId: "188732223564",
-      appId: "1:188732223564:web:734881810e32c41d0504e9"
-    },
+		options: {
+			apiKey: "AIzaSyDpqktdOQyijnyCaiaOl3_DxUQhTu3PjUg",
+			authDomain: "beeoclock-develop.firebaseapp.com",
+			projectId: "beeoclock-develop",
+			storageBucket: "beeoclock-develop.appspot.com",
+			messagingSenderId: "957741407419",
+			appId: "1:957741407419:web:cfa84b1e0ac458dfaa5d9e"
+		},
     emulator: {
       all: false,
       authorization: true,
