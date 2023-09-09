18dbade2f1dde2d033125961caaf9581e11f8bfd
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-09 11:07:52 +0200

    Add logout service and use it at error interceptor to logout user if backend will say 401 http error, also add enum for http statuses

--- src/module/utility/interceptors/error.interceptor.ts
+++ src/module/utility/interceptors/error.interceptor.ts
@@ -2,6 +2,8 @@ import {HttpErrorResponse, HttpInterceptorFn} from "@angular/common/http";
 import {catchError, throwError} from 'rxjs';
 import {inject} from "@angular/core";
 import {ToastController} from "@ionic/angular";
+import {HttpStatusEnum} from "@utility/domain/enum/http-status.enum";
+import {LogoutService} from "@utility/presentation/component/logout/logout.service";
 
 /**
  * TODO Handle any error on response
@@ -13,12 +15,18 @@ import {ToastController} from "@ionic/angular";
 export const ErrorInterceptor: HttpInterceptorFn = (request, next) => {
 
   const toastController = inject(ToastController);
+	const logoutService = inject(LogoutService);
 
   return next(request).pipe(
     catchError((response: HttpErrorResponse) => {
       const {error} = response;
+			const {statusCode} = error;
 
-      toastController.create({
+			if (statusCode === HttpStatusEnum.Unauthorized) {
+				logoutService.logout();
+			}
+
+			toastController.create({
         header: error?.error ?? 'Error',
         message: error?.message ?? 'Unknown',
         duration: 10_000,
---
+++ src/module/utility/presentation/component/logout/logout.service.ts
@@ -0,0 +1,22 @@
+import {inject, Injectable} from "@angular/core";
+import {Router} from "@angular/router";
+import {Auth} from "@angular/fire/auth";
+
+@Injectable({
+	providedIn: 'root'
+})
+export class LogoutService {
+
+	public readonly router = inject(Router);
+	public readonly auth = inject(Auth);
+
+	public logout(): void {
+		this.auth.signOut()
+			.then(() => {
+				this.router.navigate(['/'])
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+}
