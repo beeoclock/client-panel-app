e8796c50852dbf562f530c22c56c7433151ee482
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-18 00:10:57 +0200

    Fix uix

--- src/module/utility/presentation/component/modal-select-service/modal-select-service.component.ts
+++ src/module/utility/presentation/component/modal-select-service/modal-select-service.component.ts
@@ -24,15 +24,12 @@ import {HumanizeDurationPipe} from "@utility/presentation/pipes/humanize-duratio
 	template: `
 		<div class="flex flex-col gap-4">
 
-			<ng-container *ngFor="let item of modalSelectServiceListAdapter.tableState.items; let index = index">
-
-				<hr class="my-2 dark:border-beeDarkColor-700" *ngIf="index > 0">
-
-				<div class="flex flex-col gap-3 dark:text-white">
-
-					<div class="grid grid-cols-16 gap-4">
-						<div class="col-span-3 pt-1">
-							<ng-container *ngIf="item?.presentation?.banners?.length; else DefaultServiceImageTemplate">
+			<ul class="grid w-full gap-6 md:grid-cols-1">
+				<li *ngFor="let item of modalSelectServiceListAdapter.tableState.items; let index = index">
+					<input type="radio" id="modal-select-service-{{ index }}" class="hidden peer" (change)="select(item)">
+					<label for="modal-select-service-{{ index }}" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-2xl cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+						<div class="flex gap-3 mr-3">
+							<div class="">
 								<img
 									bocMedia
 									[src]="item.presentation.banners[0]"
@@ -40,53 +37,87 @@ import {HumanizeDurationPipe} from "@utility/presentation/pipes/humanize-duratio
 									twWidth="w-[90px]"
 									class="hidden w-[90px] h-[90px] rounded-2xl object-cover"
 									alt="Image of service">
-							</ng-container>
-							<ng-template #DefaultServiceImageTemplate>
-								<div class="w-[90px] h-[90px] bg-beeColor-300 rounded-2xl"></div>
-							</ng-template>
-						</div>
-						<div class="col-span-10 flex flex-col gap-2">
-							<div class="flex flex-col gap-2">
-              <span class="font-bold">
-                {{ item.languageVersions[0].title }}
-              </span>
-								<span class="text-sm text-beeColor-500">
-                {{ item.languageVersions[0].description }}
-              </span>
+							</div>
+							<div class="flex flex-col flex-1">
+								<div class="w-full text-lg font-semibold">{{ item.languageVersions[0].title }}</div>
+								<div class="w-full hidden lg:block">{{ item.languageVersions[0].description }}</div>
 							</div>
 						</div>
-						<div class="col-span-3 flex flex-col gap-3">
-
-							<button
-								*ngIf="isSelected(item)"
-								(click)="deselect(item)"
-								class="text-sm flex justify-between gap-2 w-full border border-green-200 bg-green-50 text-green-600 dark:bg-green-900/50 dark:border-green-900 px-2.5 py-1 rounded-2xl">
-								<i class="bi bi-check-circle"></i>
-								{{ 'keyword.capitalize.selected' | translate }}
-							</button>
-							<button
-								*ngIf="isNotSelected(item)"
-								(click)="select(item)"
-								class="text-sm flex justify-between gap-2 w-full border border-blue-200 bg-blue-50 text-blue-600 dark:text-blue-300 dark:bg-blue-900/50 dark:border-blue-900 px-2.5 py-1 rounded-2xl">
-								<i class="bi bi-circle"></i>
-								{{ 'keyword.capitalize.select' | translate }}
-							</button>
-
-
-							<div class="flex flex-col items-center">
-                <span class="text-end text-sm">
+						<div class="flex flex-col items-center">
+                <span class="text-end text-sm whitespace-nowrap">
                 {{ item.durationVersions[0].prices[0].price | currency: item.durationVersions[0].prices[0].currency: 'symbol-narrow' }}
               </span>
-								<span class="text-end text-sm">
+							<span class="text-end text-sm whitespace-nowrap">
                 {{ item.durationVersions[0].duration | humanizeDuration }}
               </span>
-							</div>
 						</div>
-					</div>
+					</label>
+				</li>
+			</ul>
 
-				</div>
+<!--			<ng-container>-->
 
-			</ng-container>
+<!--				<hr class="my-2 dark:border-beeDarkColor-700" *ngIf="index > 0">-->
+
+<!--				<div class="flex flex-col gap-3 dark:text-white">-->
+
+<!--					<div class="grid md:grid-cols-16 gap-4">-->
+<!--						<div class="col-span-3 pt-1">-->
+<!--							<ng-container *ngIf="item?.presentation?.banners?.length; else DefaultServiceImageTemplate">-->
+<!--								<img-->
+<!--									bocMedia-->
+<!--									[src]="item.presentation.banners[0]"-->
+<!--									twHeight="h-[90px]"-->
+<!--									twWidth="w-[90px]"-->
+<!--									class="hidden w-[90px] h-[90px] rounded-2xl object-cover"-->
+<!--									alt="Image of service">-->
+<!--							</ng-container>-->
+<!--							<ng-template #DefaultServiceImageTemplate>-->
+<!--								<div class="w-[90px] h-[90px] bg-beeColor-300 rounded-2xl"></div>-->
+<!--							</ng-template>-->
+<!--						</div>-->
+<!--						<div class="col-span-10 flex flex-col gap-2">-->
+<!--							<div class="flex flex-col gap-2">-->
+<!--              <span class="font-bold">-->
+<!--                {{ item.languageVersions[0].title }}-->
+<!--              </span>-->
+<!--								<span class="text-sm text-beeColor-500">-->
+<!--                {{ item.languageVersions[0].description }}-->
+<!--              </span>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--						<div class="col-span-3 flex flex-col gap-3">-->
+
+<!--							<button-->
+<!--								*ngIf="isSelected(item)"-->
+<!--								(click)="deselect(item)"-->
+<!--								class="text-sm flex justify-between gap-2 w-full border border-green-200 bg-green-50 text-green-600 dark:bg-green-900/50 dark:border-green-900 px-2.5 py-1 rounded-2xl">-->
+<!--								<i class="bi bi-check-circle"></i>-->
+<!--								{{ 'keyword.capitalize.selected' | translate }}-->
+<!--							</button>-->
+<!--							<button-->
+<!--								*ngIf="isNotSelected(item)"-->
+<!--								(click)="select(item)"-->
+<!--								class="text-sm flex justify-between gap-2 w-full border border-blue-200 bg-blue-50 text-blue-600 dark:text-blue-300 dark:bg-blue-900/50 dark:border-blue-900 px-2.5 py-1 rounded-2xl">-->
+<!--								<i class="bi bi-circle"></i>-->
+<!--								{{ 'keyword.capitalize.select' | translate }}-->
+<!--							</button>-->
+
+
+<!--							<div class="flex flex-col items-center">-->
+<!--                <span class="text-end text-sm">-->
+<!--                {{ item.durationVersions[0].prices[0].price | currency: item.durationVersions[0].prices[0].currency: 'symbol-narrow' }}-->
+<!--              </span>-->
+<!--								<span class="text-end text-sm">-->
+<!--                {{ item.durationVersions[0].duration | humanizeDuration }}-->
+<!--              </span>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--					</div>-->
+
+<!--				</div>-->
+
+<!--			</ng-container>-->
 
 		</div>
 		<utility-loader *ngIf="modalSelectServiceListAdapter.loading$.isOn"/>
@@ -115,27 +146,27 @@ export class ModalSelectServiceComponent implements OnInit {
 		});
 	}
 
-	public select(member: IService): void {
+	public select(service: IService): void {
 		if (!this.multiple) {
 			if (this.newSelectedServiceList.length) {
 				this.newSelectedServiceList.splice(0, 1);
 			}
 		}
-		this.newSelectedServiceList.push(member);
+		this.newSelectedServiceList.push(service);
 		this.changeDetectorRef.detectChanges();
 	}
 
-	public deselect(member: IService): void {
-		this.newSelectedServiceList = this.newSelectedServiceList.filter((selectedMember: IService) => selectedMember._id !== member._id);
+	public deselect(service: IService): void {
+		this.newSelectedServiceList = this.newSelectedServiceList.filter((selectedMember: IService) => selectedMember._id !== service._id);
 		this.changeDetectorRef.detectChanges();
 	}
 
-	public isSelected(member: IService): boolean {
-		return this.newSelectedServiceList.some((selectedMember: IService) => selectedMember._id === member._id);
+	public isSelected(service: IService): boolean {
+		return this.newSelectedServiceList.some((selectedMember: IService) => selectedMember._id === service._id);
 	}
 
-	public isNotSelected(member: IService): boolean {
-		return !this.isSelected(member);
+	public isNotSelected(service: IService): boolean {
+		return !this.isSelected(service);
 	}
 
 	private async initTableState() {
--- src/module/utility/presentation/component/modal/modal.component.ts
+++ src/module/utility/presentation/component/modal/modal.component.ts
@@ -1,12 +1,12 @@
 import {
-  AfterViewInit,
-  Component,
-  ComponentRef,
-  ElementRef,
-  HostListener,
-  inject,
-  Input,
-  ViewChild
+	AfterViewInit,
+	Component,
+	ComponentRef,
+	ElementRef,
+	HostListener,
+	inject,
+	Input,
+	ViewChild
 } from "@angular/core";
 import {Modal, ModalInterface, ModalOptions} from "flowbite";
 import {NgClass, NgForOf, NgIf} from "@angular/common";
@@ -52,11 +52,11 @@ export interface ModalButtonInterface {
       data-modal-backdrop="static"
       tabindex="-1"
       aria-hidden="true"
-      class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      class="fixed top-0 left-0 right-0 z-50 hidden w-full md:p-4 overflow-x-hidden overflow-y-auto md:inset-0 max-h-full">
       <div class="relative w-full max-w-2xl max-h-full">
 
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-beeDarkColor-800">
+        <div class="relative bg-white rounded-lg shadow h-screen md:h-auto dark:bg-beeDarkColor-800">
 
           <!-- Modal header -->
           <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-beeDarkColor-600">
@@ -74,7 +74,7 @@ export interface ModalButtonInterface {
           </div>
 
           <!-- Modal body -->
-          <div #contentRef class="p-6 space-y-6 overflow-y-auto max-h-[calc(100vh-16rem)]">
+          <div #contentRef class="p-6 space-y-6 overflow-y-auto h-[calc(100vh-10rem)] max-h-[calc(100vh-10rem)] md:h-auto md:max-h-[calc(100vh-16rem)]">
             <ng-content></ng-content>
           </div>
 
