ddb66cf8a327c33647077efdf8dd82105e52bd78
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-18 00:23:37 +0200

    Add PWA

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from '@src/app.component';
-import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {enableProdMode, importProvidersFrom, isDevMode} from '@angular/core';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {environment} from '@src/environment/environment';
 import {connectAuthEmulator, getAuth, provideAuth} from '@angular/fire/auth';
@@ -29,6 +29,7 @@ import {provideEnvironmentNgxMask} from "ngx-mask";
 import {CACHE_TABLE_CLEAR_AFTER_MS, MAIN_CONTAINER_ID, SIDEBAR_ID} from "@src/token";
 import {TEN_MINUTES} from "@utility/domain/const/c.time";
 import {LoggerModule, NgxLoggerLevel} from "ngx-logger";
+import {provideServiceWorker} from '@angular/service-worker';
 // import '@angular/common/locales/global/pl';
 // import '@angular/common/locales/global/uk';
 
@@ -45,101 +46,77 @@ initRuntimeEnvironment();
 
 bootstrapApplication(AppComponent, {
 	providers: [
-
-		// {
-		//   provide: LOCALE_ID,
-		//   useValue: 'uk'
-		// },
-
-		{
-			provide: SIDEBAR_ID,
-			useValue: 'main-sidebar'
-		},
-
-		{
-			provide: MAIN_CONTAINER_ID,
-			useValue: 'main-container'
-		},
-
-		{
-			provide: CACHE_TABLE_CLEAR_AFTER_MS,
-			useValue: TEN_MINUTES
-		},
-
-		provideEnvironmentNgxMask(),
-
-		importProvidersFrom(
-			LoggerModule.forRoot({
-				level: NgxLoggerLevel.INFO,
-			}),
-
-			NgxsModule.forRoot([IdentityState, AppState, CacheState], {
-				developmentMode: !environment.production
-			}),
-
-			NgxsReduxDevtoolsPluginModule.forRoot({
-				disabled: environment.production
-			}),
-
-			NgxIndexedDBModule.forRoot(environment.config.database),
-			IonicModule.forRoot({
-				mode: 'ios',
-				animated: false
-			}),
-			provideFirebaseApp(() => initializeApp(environment.firebase.options)),
-			provideAuth(() => {
-				const auth = getAuth();
-				auth.setPersistence(browserLocalPersistence)
-					.catch((error) => {
-						console.error(error);
-					});
-				if (environment.firebase.emulator.all || environment.firebase.emulator.authorization) {
-					connectAuthEmulator(auth, 'http://localhost:9099');
-				}
-				return auth;
-			}),
-			TranslateModule.forRoot({
-				useDefaultLang: true,
-				defaultLanguage: environment.config.language,
-				loader: {
-					provide: TranslateLoader,
-					useFactory: HttpLoaderFactory,
-					deps: [HttpClient]
-				}
-			}),
-		),
-
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: Utility.Interceptors.NotificationInterceptor,
-			multi: true
-		},
-
-		provideHttpClient(
-			withInterceptorsFromDi(),
-			withInterceptors([
-				// Utility.Interceptors.Approval, // TODO find way how to handle firebase network!
-				// Utility.Interceptors.Loading,
-				// Utility.Interceptors.Notification,
-				// Utility.Interceptors.Error,
-
-				Utility.Interceptors.AccessTokenInterceptor,
-				Utility.Interceptors.PrepareLocalHeadersInterceptor,
-				Utility.Interceptors.ApprovalInterceptor,
-				Utility.Interceptors.ParamsReplaceInterceptor,
-				Utility.Interceptors.ErrorInterceptor,
-				Utility.Interceptors.SourceInterceptor,
-				Utility.Interceptors.ClearLocalHeadersInterceptor,
-			]),
-		),
-
-		provideRouter(
-			routes,
-			withInMemoryScrolling({
-				scrollPositionRestoration: 'enabled'
-			}),
-			// TODO check if the strategy does not slow down first download
-			withPreloading(PreloadAllModules)
-		),
-	]
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'uk'
+    // },
+    {
+        provide: SIDEBAR_ID,
+        useValue: 'main-sidebar'
+    },
+    {
+        provide: MAIN_CONTAINER_ID,
+        useValue: 'main-container'
+    },
+    {
+        provide: CACHE_TABLE_CLEAR_AFTER_MS,
+        useValue: TEN_MINUTES
+    },
+    provideEnvironmentNgxMask(),
+    importProvidersFrom(LoggerModule.forRoot({
+        level: NgxLoggerLevel.INFO,
+    }), NgxsModule.forRoot([IdentityState, AppState, CacheState], {
+        developmentMode: !environment.production
+    }), NgxsReduxDevtoolsPluginModule.forRoot({
+        disabled: environment.production
+    }), NgxIndexedDBModule.forRoot(environment.config.database), IonicModule.forRoot({
+        mode: 'ios',
+        animated: false
+    }), provideFirebaseApp(() => initializeApp(environment.firebase.options)), provideAuth(() => {
+        const auth = getAuth();
+        auth.setPersistence(browserLocalPersistence)
+            .catch((error) => {
+            console.error(error);
+        });
+        if (environment.firebase.emulator.all || environment.firebase.emulator.authorization) {
+            connectAuthEmulator(auth, 'http://localhost:9099');
+        }
+        return auth;
+    }), TranslateModule.forRoot({
+        useDefaultLang: true,
+        defaultLanguage: environment.config.language,
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })),
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: Utility.Interceptors.NotificationInterceptor,
+        multi: true
+    },
+    provideHttpClient(withInterceptorsFromDi(), withInterceptors([
+        // Utility.Interceptors.Approval, // TODO find way how to handle firebase network!
+        // Utility.Interceptors.Loading,
+        // Utility.Interceptors.Notification,
+        // Utility.Interceptors.Error,
+        Utility.Interceptors.AccessTokenInterceptor,
+        Utility.Interceptors.PrepareLocalHeadersInterceptor,
+        Utility.Interceptors.ApprovalInterceptor,
+        Utility.Interceptors.ParamsReplaceInterceptor,
+        Utility.Interceptors.ErrorInterceptor,
+        Utility.Interceptors.SourceInterceptor,
+        Utility.Interceptors.ClearLocalHeadersInterceptor,
+    ])),
+    provideRouter(routes, withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled'
+    }),
+    // TODO check if the strategy does not slow down first download
+    withPreloading(PreloadAllModules)),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 }).catch(e => console.error(e));
