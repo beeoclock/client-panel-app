a078bd464a540de3bd536e109205a42eb6e60d31
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-18 03:55:52 +0200

    Use free slots from backend to represent new date and time of event at form

--- src/module/event/presentation/component/form/select-time-slot/select-time.component.ts
+++ src/module/event/presentation/component/form/select-time-slot/select-time.component.ts
@@ -1,43 +1,62 @@
-import {ChangeDetectorRef, Component, ElementRef, inject, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, inject, Input, OnInit, ViewChild} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {DateTime, Settings} from "luxon";
 import {TranslateService} from "@ngx-translate/core";
-import {NgClass, NgForOf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {Reactive} from "@utility/cdk/reactive";
+import {SlotsService} from "@event/presentation/component/form/select-time-slot/slots.service";
+import {BooleanState} from "@utility/domain";
+import {LoaderComponent} from "@utility/presentation/component/loader/loader.component";
 
 export interface ITimeSlot {
   isPast: boolean;
   datetime: DateTime;
 }
 
+const ONE_HOUR_IN_MINUTES = 60;
+const DEFAULT_INTERVAL_IN_MINUTES = 10;
+
 @Component({
   selector: 'event-select-time-slot-time-form-component',
   standalone: true,
-  imports: [
-    NgForOf,
-    NgClass
-  ],
+	imports: [
+		NgForOf,
+		NgClass,
+		LoaderComponent,
+		NgIf
+	],
   template: `
-    <div class="flex items-center justify-between gap-1">
-      <button type="button" (click)="prevSlotPack()"
-              class="px-3 py-2 hover:bg-beeColor-300 dark:hover:bg-beeDarkColor-800 cursor-pointer rounded-2xl dark:text-white">
-        <i class="bi bi-chevron-left"></i>
-      </button>
-      <div #timeSlotsContainer class="grid grid-cols-3 md:grid-cols-6 gap-1 w-full">
-        <button
-          *ngFor="let timeSlot of timeSlotList"
-          (click)="selectDateItem(timeSlot.datetime)"
-          [ngClass]="getClassList(isSelected(timeSlot.datetime))"
-          class="min-w-[72px] max-w-[72px] flex flex-col items-center justify-center border rounded-md px-3 py-2">
-          <span>{{ timeSlot.datetime.toFormat('HH:mm') }}</span>
-        </button>
-      </div>
-      <button type="button" (click)="nextSlotPack()"
-              class="px-3 py-2 hover:bg-beeColor-300 dark:hover:bg-beeDarkColor-800 cursor-pointer rounded-2xl dark:text-white">
-        <i class="bi bi-chevron-right"></i>
-      </button>
-    </div>
-  `
+		<div class="flex items-center justify-between gap-1">
+			<div *ngIf="loader.isOn" class="flex justify-center w-full">
+				<utility-loader/>
+			</div>
+			<ng-template [ngIf]="loader.isOff">
+				<button
+					(click)="prevSlotPack()"
+					[disabled]="isFirstSlotPack"
+					type="button"
+					class="px-3 py-2 hover:bg-beeColor-300 dark:hover:bg-beeDarkColor-800 cursor-pointer rounded-2xl dark:text-white">
+					<i class="bi bi-chevron-left"></i>
+				</button>
+				<div #timeSlotsContainer class="grid grid-cols-3 md:grid-cols-6 gap-1 w-full">
+					<button
+						*ngFor="let timeSlot of timeSlotLists[currentIndexListOfSlots]"
+						(click)="selectDateItem(timeSlot.datetime)"
+						[ngClass]="getClassList(isSelected(timeSlot.datetime))"
+						class="min-w-[72px] max-w-[72px] flex flex-col items-center justify-center border rounded-md px-3 py-2">
+						<span>{{ timeSlot.datetime.toFormat('HH:mm') }}</span>
+					</button>
+				</div>
+				<button
+					(click)="nextSlotPack()"
+					[disabled]="isLastSlotPack"
+					type="button"
+					class="px-3 py-2 hover:bg-beeColor-300 dark:hover:bg-beeDarkColor-800 cursor-pointer rounded-2xl dark:text-white">
+					<i class="bi bi-chevron-right"></i>
+				</button>
+			</ng-template>
+		</div>
+	`
 })
 export class SelectTimeComponent extends Reactive implements OnInit {
 
@@ -48,15 +67,18 @@ export class SelectTimeComponent extends Reactive implements OnInit {
   public localDateTimeControl!: FormControl<DateTime>;
 
   public selectedDateTime = DateTime.now();
+  public currentIndexListOfSlots = 0;
 
-  public timeSlotList: ITimeSlot[] = [];
-  public amountOfDaySlotsInContainer = 6;
+  public readonly timeSlotLists: ITimeSlot[][] = [];
+  public readonly amountOfDaySlotsInContainer = ONE_HOUR_IN_MINUTES / DEFAULT_INTERVAL_IN_MINUTES;
 
   @ViewChild('timeSlotsContainer')
   public timeSlotsContainer!: ElementRef<HTMLDivElement>;
 
-  public readonly changeDetectorRef = inject(ChangeDetectorRef);
   public readonly translateService = inject(TranslateService);
+  public readonly slotsService = inject(SlotsService);
+
+  public readonly loader = new BooleanState(true);
 
   public ngOnInit(): void {
     Settings.defaultLocale = this.translateService.currentLang;
@@ -71,63 +93,88 @@ export class SelectTimeComponent extends Reactive implements OnInit {
     this.control.valueChanges.pipe(this.takeUntil()).subscribe((VALUE) => {
       this.selectedDateTime = DateTime.fromISO(VALUE);
       this.localDateTimeControl.patchValue(this.selectedDateTime);
-      this.changeDetectorRef.detectChanges();
     });
 
     // Prepare datetime list
-    this.generateTimeSlots(this.selectedDateTime);
+		this.prepareSlots(this.selectedDateTime).then(() => {
+			this.loader.switchOff();
+		});
 
     this.localDateTimeControl.valueChanges.subscribe((value) => {
-      if (value.hasSame(this.selectedDateTime, 'day')) {
-        this.generateTimeSlots(this.selectedDateTime);
-      } else {
-        this.generateTimeSlots(value.set({
-          hour: 7
-        }));
-      }
+			this.loader.switchOn();
+			console.log(this.loader.isOn);
+			this.prepareSlots(value).then(() => {
+				this.loader.switchOff();
+			});
     });
   }
 
-  public prevSlotPack(): void {
-    this.generateTimeSlots(this.timeSlotList[0].datetime.minus({
-      hours: 1
-    }));
+	private async prepareSlots(target: DateTime): Promise<void> {
+		const start = target.startOf('day').toUTC().toISO();
+		const end = target.endOf('day').toUTC().toISO();
+		if (start && end) {
+			await this.slotsService.initSlots(start, end);
 
+			const selectedDateTime = target.toUTC().toISO();
+			if (selectedDateTime) {
+				this.initTimeSlotLists(selectedDateTime);
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @param selectedDateTime - ISO string
+	 * @private
+	 */
+	private initTimeSlotLists(selectedDateTime: string): void {
+
+		this.timeSlotLists.length = 0;
+
+		let localTemporaryList: ITimeSlot[] = [];
+
+		this.slotsService.getSlots()
+			.map((slot) => ({
+				isPast: DateTime.fromISO(slot).startOf('minute').toMillis() < DateTime.now().startOf('minute').toMillis(),
+				datetime: DateTime.fromISO(slot)
+			}))
+			.forEach((slot, index) => {
+
+				if (slot.datetime.toUTC().toISO() === selectedDateTime) {
+					this.currentIndexListOfSlots = this.timeSlotLists.length;
+				}
+
+				localTemporaryList.push(slot);
+
+				if (index !== 0 && !((index + 1) % this.amountOfDaySlotsInContainer)) {
+					this.timeSlotLists.push(localTemporaryList);
+					localTemporaryList = [];
+				}
+
+			});
+
+	}
+
+	public get isLastSlotPack(): boolean {
+		return this.currentIndexListOfSlots === (this.timeSlotLists.length - 1);
+	}
+
+	public get isFirstSlotPack(): boolean {
+		return this.currentIndexListOfSlots === 0;
+	}
+
+  public prevSlotPack(): void {
+		if ((this.currentIndexListOfSlots - 1) === -1) {
+			return;
+		}
+		this.currentIndexListOfSlots = this.currentIndexListOfSlots - 1;
   }
 
   public nextSlotPack(): void {
-    const item = this.timeSlotList.at(-1);
-    if (item) {
-      this.generateTimeSlots(item.datetime.plus({
-        minute: 10
-      }));
-    }
-
-  }
-
-  public generateTimeSlot(sourceDatetime: DateTime, time: number): ITimeSlot {
-    // TODO performance
-    const datetime = sourceDatetime.plus({
-      minutes: 10 * time
-    });
-
-    return {
-      isPast: datetime.startOf('minute').toMillis() < DateTime.now().startOf('minute').toMillis(),
-      datetime
-    };
-  }
-
-  public generateTimeSlots(sourceDatetime: DateTime): void {
-    this.timeSlotList = [];
-    sourceDatetime = sourceDatetime.setLocale(this.translateService.currentLang);
-    sourceDatetime = sourceDatetime.set({
-      minute: 0,
-      second: 0,
-    });
-    for (let time = 0; time < this.amountOfDaySlotsInContainer; time++) {
-      this.timeSlotList.push(this.generateTimeSlot(sourceDatetime, time));
-    }
-    this.changeDetectorRef.detectChanges();
+		if ((this.currentIndexListOfSlots + 1) === this.timeSlotLists.length) {
+			return;
+		}
+		this.currentIndexListOfSlots = this.currentIndexListOfSlots + 1;
   }
 
   public getClassList(isSelected: boolean): string[] {
--- src/module/event/presentation/component/form/select-time-slot/slots.service.ts
+++ src/module/event/presentation/component/form/select-time-slot/slots.service.ts
@@ -3,17 +3,42 @@ import {SlotsEventApiAdapter} from "@event/adapter/external/api/slots.event.api.
 
 @Injectable()
 export class SlotsService {
+
 	private readonly slotsEventApiAdapter = inject(SlotsEventApiAdapter);
-	constructor() {
-		const start = new Date();
-		start.setHours(0, 0, 0, 0);
-		const end = new Date();
-		end.setHours(23, 59, 59, 999);
-		this.slotsEventApiAdapter.executeAsync({
-			start: start.toISOString(),
-			end: end.toISOString(),
+	private readonly localTemporaryCache = new Map<string, string[]>();
+	private readonly slots: string[] = [];
+
+	public getSlots(): string[] {
+
+		return structuredClone(this.slots);
+
+	}
+
+	/**
+	 *
+	 * @param start - ISO string
+	 * @param end - ISO string
+	 */
+	public async initSlots(start: string, end: string) {
+
+		const key = `${start}-${end}`;
+		if (this.localTemporaryCache.has(key)) {
+			this.slots.length = 0;
+			this.slots.push(...(this.localTemporaryCache.get(key) ?? []));
+			return;
+		}
+
+		const slots = await this.slotsEventApiAdapter.executeAsync({
+			start,
+			end,
 			eventDurationMinutes: 60,
 			slotIntervalMinutes: 10
 		});
+
+		this.localTemporaryCache.set(key, slots);
+		this.slots.length = 0;
+		this.slots.push(...slots);
+
 	}
+
 }
