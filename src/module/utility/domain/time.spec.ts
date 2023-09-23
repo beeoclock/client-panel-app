f6345550df11d8dcca590b1c5261c62d1a84422d
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-23 11:07:25 +0200

    fix ut

--- src/module/utility/domain/time.spec.ts
+++ src/module/utility/domain/time.spec.ts
@@ -1,57 +1,43 @@
-import {formatTime, reverseFormatTime} from "@utility/domain/time";
+import {extractSecondsFrom_hh_mm_ss, secondsTo_hh_mm_ss} from "@utility/domain/time";
 
 describe('formatTime', () => {
   it('should format time correctly for 0 seconds', () => {
     const timeInSeconds = 0;
-    const formattedTime = formatTime(timeInSeconds);
+    const formattedTime = secondsTo_hh_mm_ss(timeInSeconds);
     expect(formattedTime).toBe('00:00:00');
   });
 
   it('should format time correctly for 3661 seconds', () => {
     const timeInSeconds = 3661;
-    const formattedTime = formatTime(timeInSeconds);
+    const formattedTime = secondsTo_hh_mm_ss(timeInSeconds);
     expect(formattedTime).toBe('01:01:01');
   });
 
   it('should format time correctly for 7200 seconds', () => {
     const timeInSeconds = 7200;
-    const formattedTime = formatTime(timeInSeconds);
+    const formattedTime = secondsTo_hh_mm_ss(timeInSeconds);
     expect(formattedTime).toBe('02:00:00');
   });
 
   it('should format seconds to hh:mm:ss', () => {
-    expect(formatTime(3661, 'seconds')).toBe('01:01:01');
-  });
-
-  it('should format minutes to hh:mm:ss', () => {
-    expect(formatTime(90, 'minutes')).toBe('01:30:00');
-  });
-
-  it('should format hours to hh:mm:ss', () => {
-    expect(formatTime(3.5, 'hours')).toBe('03:30:00');
-  });
-
-  it('should throw an error for invalid time unit', () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    expect(() => formatTime(100, 'days')).toThrow('Invalid time unit: days');
+    expect(secondsTo_hh_mm_ss(3661)).toBe('01:01:01');
   });
 
   it('should reverse formatted time "00:00:00" to 0 seconds', () => {
     const formattedTime = '00:00:00';
-    const seconds = reverseFormatTime(formattedTime);
+    const seconds = extractSecondsFrom_hh_mm_ss(formattedTime);
     expect(seconds).toBe(0);
   });
 
   it('should reverse formatted time "01:30:00" to 5400 seconds', () => {
     const formattedTime = '01:30:00';
-    const seconds = reverseFormatTime(formattedTime);
+    const seconds = extractSecondsFrom_hh_mm_ss(formattedTime);
     expect(seconds).toBe(5400);
   });
 
   it('should reverse formatted time "03:45:15" to 13515 seconds', () => {
     const formattedTime = '03:45:15';
-    const seconds = reverseFormatTime(formattedTime);
+    const seconds = extractSecondsFrom_hh_mm_ss(formattedTime);
     expect(seconds).toBe(13515);
   });
 
