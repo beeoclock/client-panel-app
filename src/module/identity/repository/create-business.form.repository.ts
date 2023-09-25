57005c506ce6eba93f8e266ef6bb2e6754de9ded
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-25 20:36:26 +0200

    Use local storage to store data and use the data to set to form from storage and add logger

--- src/module/identity/repository/create-business.form.repository.ts
+++ src/module/identity/repository/create-business.form.repository.ts
@@ -1,5 +1,7 @@
 import {Injectable} from "@angular/core";
 import CreateBusinessForm from "@identity/presentation/form/create-business.form";
+import {BooleanState} from "@utility/domain";
+import {NGXLogger} from "ngx-logger";
 
 @Injectable({
 	providedIn: 'root'
@@ -8,16 +10,26 @@ export class CreateBusinessFormRepository {
 
 	public readonly form = new CreateBusinessForm();
 	public readonly formLocalStorageKey = 'create-business-form';
+	public readonly initializedValueFromStorage = new BooleanState(false);
 
-	constructor() {
+	constructor(
+		private readonly logger: NGXLogger
+	) {
 		this.initHandlers();
+		if (this.initializedValueFromStorage.isOff) {
+			this.initValueFromLocalStorage();
+		}
 	}
 
 	public initValueFromLocalStorage(): void {
+		this.logger.debug('CreateBusinessFormRepository.initValueFromLocalStorage');
 		const value = localStorage.getItem(this.formLocalStorageKey);
 		if (value) {
-			this.form.patchValue(JSON.parse(value));
+			const parsedValue = JSON.parse(value);
+			this.logger.debug('CreateBusinessFormRepository.initValueFromLocalStorage', parsedValue);
+			this.form.patchValue(parsedValue);
 		}
+		this.initializedValueFromStorage.switchOn();
 	}
 
 	public clearLocalStorage(): void {
@@ -25,12 +37,12 @@ export class CreateBusinessFormRepository {
 	}
 
 	private initHandlers(): void {
+		this.logger.debug('CreateBusinessFormRepository.initHandlers');
 		this.initFormValueHandler();
 	}
 
 	private initFormValueHandler(): void {
 		this.form.valueChanges.subscribe((value) => {
-			console.log(value);
 			localStorage.setItem(this.formLocalStorageKey, JSON.stringify(value));
 		});
 	}
