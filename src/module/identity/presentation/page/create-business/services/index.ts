c5926c038fe191ddb513f907237b9d9656b30406
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-26 09:18:51 +0200

    Prepare Services step

--- src/module/identity/presentation/page/create-business/services/index.ts
+++ src/module/identity/presentation/page/create-business/services/index.ts
@@ -1,10 +1,12 @@
-import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, ViewEncapsulation} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {PrimaryLinkButtonDirective} from "@utility/presentation/directives/button/primary.link.button.directive";
 import {FormInputComponent} from "@utility/presentation/component/input/form.input.component";
 import {PrimaryButtonDirective} from "@utility/presentation/directives/button/primary.button.directive";
 import {BackLinkComponent} from "@utility/presentation/component/link/back.link.component";
 import {ChangeLanguageComponent} from "@utility/presentation/component/change-language/change-language.component";
+import {TranslateModule} from "@ngx-translate/core";
+import {CreateBusinessQuery} from "@identity/query/create-business.query";
 
 @Component({
 	selector: 'identity-create-business-schedules-page',
@@ -17,11 +19,14 @@ import {ChangeLanguageComponent} from "@utility/presentation/component/change-la
         FormInputComponent,
         PrimaryButtonDirective,
         BackLinkComponent,
-        ChangeLanguageComponent
+        ChangeLanguageComponent,
+        TranslateModule
     ],
 	encapsulation: ViewEncapsulation.None
 })
 export default class Index {
 
+	private readonly createBusinessQuery = inject(CreateBusinessQuery);
+	public readonly servicesForm = this.createBusinessQuery.getServicesForm();
 
 }
