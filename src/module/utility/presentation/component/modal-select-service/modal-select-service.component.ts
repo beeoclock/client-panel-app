a7af879601c59d1b4cd720cf12c53e43c649880f
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-09-27 16:09:26 +0200

    Add new component at service module to show item at list of services

--- src/module/utility/presentation/component/modal-select-service/modal-select-service.component.ts
+++ src/module/utility/presentation/component/modal-select-service/modal-select-service.component.ts
@@ -10,6 +10,7 @@ import {PrimaryButtonDirective} from "@utility/presentation/directives/button/pr
 import {Router, RouterLink} from "@angular/router";
 import {ModalComponent} from "@utility/presentation/component/modal/modal.component";
 import {NGXLogger} from "ngx-logger";
+import {ServiceItemComponent} from "@service/presentation/component/list/item/item.componen";
 
 @Component({
 	selector: 'utility-modal-select-service-component',
@@ -25,7 +26,8 @@ import {NGXLogger} from "ngx-logger";
 		BocMediaDirective,
 		HumanizeDurationPipe,
 		PrimaryButtonDirective,
-		RouterLink
+		RouterLink,
+		ServiceItemComponent
 	],
 	template: `
 		<div class="flex flex-col gap-4">
@@ -41,97 +43,20 @@ import {NGXLogger} from "ngx-logger";
 			<ul class="grid w-full gap-6 md:grid-cols-1">
 				<li *ngFor="let item of modalSelectServiceListAdapter.tableState.items; let index = index">
 					<input type="radio" id="modal-select-service-{{ index }}" [checked]="isSelected(item)" name="modalSelectService" class="hidden peer" (change)="select(item)">
-					<label for="modal-select-service-{{ index }}" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-2xl cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
-						<div class="flex gap-3 mr-3">
-							<div class="">
-								<img
-									bocMedia
-									[src]="item?.presentation?.banners?.[0] ?? ''"
-									twHeight="h-[90px]"
-									twWidth="w-[90px]"
-									class="hidden w-[90px] h-[90px] rounded-2xl object-cover"
-									alt="Image of service">
-							</div>
-							<div class="flex flex-col flex-1">
-								<div class="w-full text-lg font-semibold">{{ item.languageVersions[0].title }}</div>
-								<div class="w-full hidden lg:block">{{ item.languageVersions[0].description }}</div>
-							</div>
-						</div>
-						<div class="flex flex-col items-center">
-                <span class="text-end text-sm whitespace-nowrap">
-                {{ item.durationVersions[0].prices[0].price | currency: item.durationVersions[0].prices[0].currency: 'symbol-narrow' }}
-              </span>
-							<span class="text-end text-sm whitespace-nowrap">
-                {{ item.durationVersions[0].durationInSeconds | humanizeDuration }}
-              </span>
-						</div>
+					<label
+						for="modal-select-service-{{ index }}"
+						class="
+							inline-flex
+							cursor-pointer
+							rounded-2xl
+							bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 peer-checked:bg-blue-50
+							border-4 border-gray-200 dark:border-gray-700 peer-checked:border-blue-200
+							dark:hover:text-gray-300 dark:peer-checked:text-blue-500 peer-checked:text-blue-600 hover:text-gray-600
+						">
+						<service-item-component [item]="item"/>
 					</label>
 				</li>
 			</ul>
-
-<!--			<ng-container>-->
-
-<!--				<hr class="my-2 dark:border-beeDarkColor-700" *ngIf="index > 0">-->
-
-<!--				<div class="flex flex-col gap-3 dark:text-white">-->
-
-<!--					<div class="grid md:grid-cols-16 gap-4">-->
-<!--						<div class="col-span-3 pt-1">-->
-<!--							<ng-container *ngIf="item?.presentation?.banners?.length; else DefaultServiceImageTemplate">-->
-<!--								<img-->
-<!--									bocMedia-->
-<!--									[src]="item.presentation.banners[0]"-->
-<!--									twHeight="h-[90px]"-->
-<!--									twWidth="w-[90px]"-->
-<!--									class="hidden w-[90px] h-[90px] rounded-2xl object-cover"-->
-<!--									alt="Image of service">-->
-<!--							</ng-container>-->
-<!--							<ng-template #DefaultServiceImageTemplate>-->
-<!--								<div class="w-[90px] h-[90px] bg-beeColor-300 rounded-2xl"></div>-->
-<!--							</ng-template>-->
-<!--						</div>-->
-<!--						<div class="col-span-10 flex flex-col gap-2">-->
-<!--							<div class="flex flex-col gap-2">-->
-<!--              <span class="font-bold">-->
-<!--                {{ item.languageVersions[0].title }}-->
-<!--              </span>-->
-<!--								<span class="text-sm text-beeColor-500">-->
-<!--                {{ item.languageVersions[0].description }}-->
-<!--              </span>-->
-<!--							</div>-->
-<!--						</div>-->
-<!--						<div class="col-span-3 flex flex-col gap-3">-->
-
-<!--							<button-->
-<!--								*ngIf="isSelected(item)"-->
-<!--								(click)="deselect(item)"-->
-<!--								class="text-sm flex justify-between gap-2 w-full border border-green-200 bg-green-50 text-green-600 dark:bg-green-900/50 dark:border-green-900 px-2.5 py-1 rounded-2xl">-->
-<!--								<i class="bi bi-check-circle"></i>-->
-<!--								{{ 'keyword.capitalize.selected' | translate }}-->
-<!--							</button>-->
-<!--							<button-->
-<!--								*ngIf="isNotSelected(item)"-->
-<!--								(click)="select(item)"-->
-<!--								class="text-sm flex justify-between gap-2 w-full border border-blue-200 bg-blue-50 text-blue-600 dark:text-blue-300 dark:bg-blue-900/50 dark:border-blue-900 px-2.5 py-1 rounded-2xl">-->
-<!--								<i class="bi bi-circle"></i>-->
-<!--								{{ 'keyword.capitalize.select' | translate }}-->
-<!--							</button>-->
-
-
-<!--							<div class="flex flex-col items-center">-->
-<!--                <span class="text-end text-sm">-->
-<!--                {{ item.durationVersions[0].prices[0].price | currency: item.durationVersions[0].prices[0].currency: 'symbol-narrow' }}-->
-<!--              </span>-->
-<!--								<span class="text-end text-sm">-->
-<!--                {{ item.durationVersions[0].durationInSeconds | humanizeDuration }}-->
-<!--              </span>-->
-<!--							</div>-->
-<!--						</div>-->
-<!--					</div>-->
-
-<!--				</div>-->
-
-<!--			</ng-container>-->
 
 		</div>
 		<utility-loader *ngIf="modalSelectServiceListAdapter.loading$.isOn"/>
