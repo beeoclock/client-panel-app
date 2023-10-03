483f08dd01f115cc865c141d6568e11917751cd2
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2023-10-03 11:18:19 +0200

    Improve page to send again email to confirm email address

--- src/module/identity/presentation/page/confirm-email/index.ts
+++ src/module/identity/presentation/page/confirm-email/index.ts
@@ -1,5 +1,5 @@
 import {Component, inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import {applyActionCode, Auth} from '@angular/fire/auth';
 import {AsyncPipe, NgIf, NgOptimizedImage} from "@angular/common";
 import {NGXLogger} from "ngx-logger";
@@ -7,7 +7,13 @@ import {Select, Store} from "@ngxs/store";
 import {IdentityActions} from "@identity/state/identity/identity.actions";
 import {SendConfirmEmailListApiAdapter} from "@identity/adapter/external/api/send-confirm-email-list.api.adapter";
 import {IdentityState} from "@identity/state/identity/identity.state";
-import {firstValueFrom, Observable} from "rxjs";
+import {filter, firstValueFrom, Observable} from "rxjs";
+import {CardComponent} from "@utility/presentation/component/card/card.component";
+import {TranslateModule} from "@ngx-translate/core";
+import {PrimaryButtonDirective} from "@utility/presentation/directives/button/primary.button.directive";
+import {Reactive} from "@utility/cdk/reactive";
+import {BooleanState} from "@utility/domain";
+import {PrimaryLinkStyleDirective} from "@utility/presentation/directives/link/primary.link.style.directive";
 
 @Component({
 	selector: 'identity-confirm-email-page',
@@ -16,18 +22,27 @@ import {firstValueFrom, Observable} from "rxjs";
 	imports: [
 		NgOptimizedImage,
 		NgIf,
-		AsyncPipe
+		AsyncPipe,
+		CardComponent,
+		TranslateModule,
+		PrimaryButtonDirective,
+		PrimaryLinkStyleDirective
 	],
 	encapsulation: ViewEncapsulation.None
 })
-export default class Index implements OnInit {
+export default class Index extends Reactive implements OnInit {
 
 	private readonly firebaseMode = 'verifyEmail' as const;
 	private readonly auth = inject(Auth);
 	private readonly activatedRoute = inject(ActivatedRoute);
 	private readonly logger = inject(NGXLogger);
 	private readonly store = inject(Store);
+	private readonly router = inject(Router);
 	private readonly sendConfirmEmailListApiAdapter = inject(SendConfirmEmailListApiAdapter);
+
+	public readonly emailSending = new BooleanState(false);
+	public readonly emailIsSent = new BooleanState(false);
+	public emailUrl = new URL('https://beeoclock.com');
 
 	public readonly isAuthorized$ = this.store.select((state) => {
 		return state.identity.token !== undefined;
@@ -36,9 +51,14 @@ export default class Index implements OnInit {
 	@Select(IdentityState.accountEmail)
 	accountEmail$!: Observable<unknown>;
 
+	constructor() {
+		super();
+	}
+
+
 	public ngOnInit(): void {
 		this.store.dispatch(new IdentityActions.InitToken())
-		this.activatedRoute.queryParams.subscribe((params: Params | {
+		this.activatedRoute.queryParams.pipe(this.takeUntil()).subscribe((params: Params | {
 			mode?: string,
 			oobCode?: string,
 			continueUrl?: string,
@@ -47,18 +67,36 @@ export default class Index implements OnInit {
 			if (params?.mode === this.firebaseMode) {
 				applyActionCode(this.auth, params.oobCode)
 					.then(() => {
-						this.logger.info('E-mail is verified');
+						this.logger.debug('email is sent!');
 					})
 					.catch((error) => {
 						this.logger.error(error);
 					});
 			}
 		});
+		this.store.select(IdentityState.accountEmailIsVerified).pipe(
+			this.takeUntil(),
+			filter((isVerified) => !!isVerified),
+		).subscribe(() => {
+			this.router.navigate(['/']).then();
+		});
 	}
 
 	public async sendAgain() {
+		this.emailSending.switchOn();
 		const email = await firstValueFrom(this.accountEmail$);
-		this.sendConfirmEmailListApiAdapter.executeAsync({email}).then();
+		if (typeof email === 'string') {
+			this.emailUrl = new URL(`https://${email.split('@')[1]}`);
+			try {
+				await this.sendConfirmEmailListApiAdapter.executeAsync({email});
+				this.emailIsSent.switchOn();
+			} catch (e) {
+				this.logger.error(e);
+			}
+			this.emailSending.switchOff();
+		} else {
+			this.logger.error('email is not string', email);
+		}
 	}
 
 }
