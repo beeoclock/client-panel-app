0e76f09f3c763c922b87febf59d0aebeee3e0013
Karba <ivan.karbashevskyi@gmail.com>
2023-10-30 00:50:37 +0100

    Update package

--- src/module/utility/domain/first-key-name.ts
+++ src/module/utility/domain/first-key-name.ts
@@ -1,8 +1,8 @@
 import {is} from 'thiis';
 
-export function getFirstKey(value: any): string {
-  if (is.object_not_empty(value)) {
-    return Object.keys(value)[0];
-  }
-  return '';
+export function getFirstKey(value: unknown): string {
+	if (is.object_not_empty<Object>(value)) {
+		return Object.keys(value)[0];
+	}
+	return '';
 }
