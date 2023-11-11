e975de4d0adc7e6ee9fb65913abd66f495a00985
Karba <ivan.karbashevskyi@gmail.com>
2023-11-11 17:37:06 +0100

    Add two new resolvers for event module to detect serviceId and customerId

---
+++ src/module/event/presentation/resolver/event.customer.resolver.ts
@@ -0,0 +1,39 @@
+import {inject} from "@angular/core";
+import {ActivatedRouteSnapshot, ResolveFn} from "@angular/router";
+import {Store} from "@ngxs/store";
+import {catchError, EMPTY, of, switchMap} from "rxjs";
+import {ItemCustomerApiAdapter} from "@customer/adapter/external/api/item.customer.api.adapter";
+import {ICustomer} from "@customer/domain";
+import {ICustomerState} from "@customer/state/customer/customer.state";
+import {CustomerActions} from "@customer/state/customer/customer.actions";
+
+export const eventCustomerResolver: ResolveFn<ICustomer | undefined> = (
+	route: ActivatedRouteSnapshot,
+) => {
+
+	const store = inject(Store); // NGXS
+	const itemCustomerApiAdapter = inject(ItemCustomerApiAdapter); // NGXS
+
+	const {customerId} = route.queryParams;
+
+	if (!customerId) {
+		return undefined;
+	}
+
+	return store
+		.selectOnce((state: { customer: ICustomerState; }) => state.customer.tableState.items)
+		.pipe(
+			switchMap((customers) => {
+				if (customers?.length) {
+					const customer = customers.find((customer: ICustomer) => customer._id === customerId);
+					if (!customer) {
+						return store.dispatch(new CustomerActions.GetItem(customerId));
+					}
+					return of(customer);
+				}
+				return itemCustomerApiAdapter.execute$(customerId);
+			}),
+			catchError(() => EMPTY),
+		);
+
+};
