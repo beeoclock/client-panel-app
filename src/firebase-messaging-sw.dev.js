a5e52ab09678af7431b087caa775ad13c840fbff
Karba <ivan.karbashevskyi@gmail.com>
2024-01-23 00:33:04 +0100

    return back prev version

--- src/firebase-messaging-sw.dev.js
+++ src/firebase-messaging-sw.dev.js
@@ -1,61 +1,11 @@
-// import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js';
-// import {
-// 	getMessaging,
-// 	isSupported,
-// 	onBackgroundMessage
-// } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging-sw.js';
-//
-// // TODO: Detect which mode we are in (dev or prod) and use the appropriate config
-// const app = initializeApp({
-// 	apiKey: "AIzaSyDpqktdOQyijnyCaiaOl3_DxUQhTu3PjUg",
-// 	authDomain: "beeoclock-develop.firebaseapp.com",
-// 	projectId: "beeoclock-develop",
-// 	storageBucket: "beeoclock-develop.appspot.com",
-// 	messagingSenderId: "957741407419",
-// 	appId: "1:957741407419:web:cfa84b1e0ac458dfaa5d9e",
-// 	vapidKey: "BHlprMakUwMYPx_Y5xSF8QeaiGOAiMPhdHKmaSTUbgWFdTEsD7Ov42yTEdQdLua4HexFD85gqO1jawbSR0Q-Jw0"
-// });
-//
-// isSupported().then(isSupported => {
-//
-// 	if (isSupported) {
-//
-// 		const messaging = getMessaging(app);
-//
-// 		onBackgroundMessage(messaging, ({ notification: { title, body, image } }) => {
-// 			self.registration.showNotification(title, { body, icon: image || '/assets/icons/icon-72x72.png' });
-// 		});
-//
-// 		self.addEventListener('notificationclick', (event) => {
-// 			console.log("[SW:NOTIFICATION] notificationclick", event)
-// 			const urlToRedirect = event.notification.data.url;
-// 			event.notification.close();
-// 			event.waitUntil(self.clients.openWindow(urlToRedirect));
-// 		});
-//
-// 		self.addEventListener('push', (event) => {
-// 			console.log("[SW:NOTIFICATION] push", event)
-// 			// if (event.data) {
-// 			// 	const dataText = event.data.text();
-// 			// 	notificationTitle = 'Custom Notification';
-// 			// 	notificationOptions.body = 'Message: ' + `${dataText}`;
-// 			// 	var title = event.data.notification.title;
-// 			// 	var message = event.data.notification.message;
-// 			// 	var icon = event.data.notification.icon;
-// 			// 	var notificationTag = event.data.notification.tag;﻿
-// 			// }
-// 		});
-//
-// 	}
-//
-// });
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js';
+import {
+	getMessaging,
+	isSupported,
+	onBackgroundMessage
+} from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging-sw.js';
 
-
-// Service Worker
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
-
-const firebaseConfig = {
+const app = initializeApp({
 	apiKey: "AIzaSyDpqktdOQyijnyCaiaOl3_DxUQhTu3PjUg",
 	authDomain: "beeoclock-develop.firebaseapp.com",
 	projectId: "beeoclock-develop",
@@ -63,22 +13,38 @@ const firebaseConfig = {
 	messagingSenderId: "957741407419",
 	appId: "1:957741407419:web:cfa84b1e0ac458dfaa5d9e",
 	vapidKey: "BHlprMakUwMYPx_Y5xSF8QeaiGOAiMPhdHKmaSTUbgWFdTEsD7Ov42yTEdQdLua4HexFD85gqO1jawbSR0Q-Jw0"
-};
+});
 
-// // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+isSupported().then(isSupported => {
 
-// Initialize Firebase Cloud Messaging and get a reference to the service
-const messaging = firebase.messaging()
+	if (isSupported) {
 
-messaging.onBackgroundMessage(payload => {
-	// Customize notification here
-	const notificationTitle = 'Background Message Title';
-	const notificationOptions = {
-		body: 'Background Message body.',
-		icon: '/assets/icons/icon-72x72.png'
-	};
+		const messaging = getMessaging(app);
 
-	self.registration.showNotification(notificationTitle,
-		notificationOptions);
+		onBackgroundMessage(messaging, ({ notification: { title, body, image } }) => {
+			self.registration.showNotification(title, { body, icon: image || '/assets/icons/icon-72x72.png' });
+		});
+
+		self.addEventListener('notificationclick', (event) => {
+			console.log("[SW:NOTIFICATION] notificationclick", event)
+			const urlToRedirect = event.notification.data.url;
+			event.notification.close();
+			event.waitUntil(self.clients.openWindow(urlToRedirect));
+		});
+
+		// self.addEventListener('push', (event) => {
+		// 	console.log("[SW:NOTIFICATION] push", event)
+		// 	// if (event.data) {
+		// 	// 	const dataText = event.data.text();
+		// 	// 	notificationTitle = 'Custom Notification';
+		// 	// 	notificationOptions.body = 'Message: ' + `${dataText}`;
+		// 	// 	var title = event.data.notification.title;
+		// 	// 	var message = event.data.notification.message;
+		// 	// 	var icon = event.data.notification.icon;
+		// 	// 	var notificationTag = event.data.notification.tag;﻿
+		// 	// }
+		// });
+
+	}
+
 });
