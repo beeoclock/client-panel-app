b59c2a6f72c4c5e77e6da80b8f663fa854aeb8b2
Karba <ivan.karbashevskyi@gmail.com>
2024-03-05 10:52:00 +0100

    Add danish

--- src/module/utility/domain/enum/language-code.enum.ts
+++ src/module/utility/domain/enum/language-code.enum.ts
@@ -2,12 +2,14 @@ export enum LanguageCodeEnum {
   en = 'en',
   pl = 'pl',
   uk = 'uk',
+  da = 'da',
 }
 
 export const LanguageRecord: Record<LanguageCodeEnum, string> = {
   [LanguageCodeEnum.en]: 'English',
   [LanguageCodeEnum.uk]: 'Українська',
   [LanguageCodeEnum.pl]: 'Polski',
+  [LanguageCodeEnum.da]: 'Dansk',
 };
 
 export const LANGUAGES: {
