85d93e0ca06a642e21bb02d90a355059aa65fd01
Karba <ivan.karbashevskyi@gmail.com>
2024-03-13 23:03:40 +0100

    fix

--- src/module/event/presentation/page/calendar-with-specialists/component/header.calendar.component.ts
+++ src/module/event/presentation/page/calendar-with-specialists/component/header.calendar.component.ts
@@ -6,12 +6,13 @@ import {NgIf} from "@angular/common";
 	selector: 'event-header-calendar-component',
 	template: `
 		<ng-container *ngIf="member">
-			<div class="flex gap-2 items-center justify-start h-full px-2 text-slate-900 dark:text-slate-200 text-sm font-medium">
+			<div
+				class="flex gap-2 items-center justify-start h-full px-2 text-slate-900 dark:text-slate-200 text-sm font-medium">
 				<div class="rounded-full bg-beeColor-400 min-h-8 min-w-8 flex justify-center items-center">
-					<div class="text-white text-xs font-bold">{{ member.firstName.charAt(0) }}</div>
-					<div class="text-white text-xs font-bold">{{ member.lastName.charAt(0) }}</div>
+					<div class="text-white text-xs font-bold">{{ member?.firstName?.[0] ?? '' }}</div>
+					<div class="text-white text-xs font-bold">{{ member?.lastName?.[0] ?? ''}}</div>
 				</div>
-				<div>{{ member.firstName }} {{ member.lastName }}</div>
+				<div>{{ member?.firstName ?? '' }} {{ member?.lastName ?? '' }}</div>
 			</div>
 		</ng-container>
 	`,
