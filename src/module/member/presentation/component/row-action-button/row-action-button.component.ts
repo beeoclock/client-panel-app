d36f9e7f30e682ba9a63cdb9a2aff9c87840ec04
Karba <ivan.karbashevskyi@gmail.com>
2024-03-16 16:23:07 +0100

    Hide btn to add event via member table

--- src/module/member/presentation/component/row-action-button/row-action-button.component.ts
+++ src/module/member/presentation/component/row-action-button/row-action-button.component.ts
@@ -13,15 +13,15 @@ import {RIMember} from "@member/domain";
 	encapsulation: ViewEncapsulation.None,
 	template: `
 		<utility-table-column-action [id]="id" (delete)="delete($event)">
-			<li>
-				<a
-					[routerLink]="['../../', 'event', 'form']"
-					[queryParams]="{serviceId: item._id, returnUrl}"
-					class="flex gap-4 text-start px-4 py-2 hover:bg-beeColor-100 dark:hover:bg-beeDarkColor-600 dark:hover:text-white">
-					<i class="bi bi-calendar2-week"></i>
-					{{ 'keyword.capitalize.add-event' | translate }}
-				</a>
-			</li>
+<!--			<li>-->
+<!--				<a-->
+<!--					[routerLink]="['../../', 'event', 'form']"-->
+<!--					[queryParams]="{serviceId: item._id, returnUrl}"-->
+<!--					class="flex gap-4 text-start px-4 py-2 hover:bg-beeColor-100 dark:hover:bg-beeDarkColor-600 dark:hover:text-white">-->
+<!--					<i class="bi bi-calendar2-week"></i>-->
+<!--					{{ 'keyword.capitalize.add-event' | translate }}-->
+<!--				</a>-->
+<!--			</li>-->
 		</utility-table-column-action>
 	`,
 	imports: [
