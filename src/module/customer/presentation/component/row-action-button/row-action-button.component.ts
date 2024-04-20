3225657e355d5444d6df5c2ee29b9202b01291ce
Karba <ivan.karbashevskyi@gmail.com>
2024-04-21 00:18:53 +0200

    fix

--- src/module/customer/presentation/component/row-action-button/row-action-button.component.ts
+++ src/module/customer/presentation/component/row-action-button/row-action-button.component.ts
@@ -74,7 +74,7 @@ export class RowActionButtonComponent {
 	}
 
 	public open(): void {
-		this.store.dispatch(new CustomerActions.OpenFormToEditById(this.item._id));
+		this.store.dispatch(new CustomerActions.OpenDetailsById(this.item._id));
 	}
 
 	public edit(): void {
