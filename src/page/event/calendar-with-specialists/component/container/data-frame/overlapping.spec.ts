7b002a84692cf0c58f4c39e8df5c777ce0f89a6c
Karba <ivan.karbashevskyi@gmail.com>
2024-05-27 18:29:23 +0200

    fix UT

--- src/page/event/calendar-with-specialists/component/container/data-frame/overlapping.spec.ts
+++ src/page/event/calendar-with-specialists/component/container/data-frame/overlapping.spec.ts
@@ -1,18 +1,18 @@
 import {groupOverlappingEvents, isOverlapping} from './eventUtils';
-import {RIEvent} from "@event/domain"; // Припустимо, що ваші функції знаходяться в цьому файлі
+import {IEvent_V2} from "@event/domain"; // Припустимо, що ваші функції знаходяться в цьому файлі
 
 describe('Event Utils', () => {
 	describe('isOverlapping', () => {
 		it('should return true when events overlap', () => {
 			const event1  = {id: 1, start: '2022-01-01T10:00:00', end: '2022-01-01T12:00:00'};
 			const event2  = {id: 2, start: '2022-01-01T11:00:00', end: '2022-01-01T13:00:00'};
-			expect(isOverlapping(event1 as unknown as RIEvent, event2 as unknown as RIEvent)).toBe(true);
+			expect(isOverlapping(event1 as unknown as IEvent_V2, event2 as unknown as IEvent_V2)).toBe(true);
 		});
 
 		it('should return false when events do not overlap', () => {
 			const event1  = {id: 1, start: '2022-01-01T10:00:00', end: '2022-01-01T11:00:00'};
 			const event2  = {id: 2, start: '2022-01-01T12:00:00', end: '2022-01-01T13:00:00'};
-			expect(isOverlapping(event1 as unknown as RIEvent, event2 as unknown as RIEvent)).toBe(false);
+			expect(isOverlapping(event1 as unknown as IEvent_V2, event2 as unknown as IEvent_V2)).toBe(false);
 		});
 	});
 
@@ -23,7 +23,7 @@ describe('Event Utils', () => {
 				{id: 2, start: '2022-01-01T11:00:00', end: '2022-01-01T13:00:00'},
 				{id: 3, start: '2022-01-01T14:00:00', end: '2022-01-01T15:00:00'},
 			];
-			const groupedEvents = groupOverlappingEvents(events as unknown as RIEvent[]);
+			const groupedEvents = groupOverlappingEvents(events as unknown as IEvent_V2[]);
 			expect(groupedEvents).toEqual([
 				[events[0], events[1]],
 				[events[2]],
@@ -36,7 +36,7 @@ describe('Event Utils', () => {
 				{id: 2, start: '2022-01-01T12:00:00', end: '2022-01-01T13:00:00'},
 				{id: 3, start: '2022-01-01T14:00:00', end: '2022-01-01T15:00:00'},
 			];
-			const groupedEvents = groupOverlappingEvents(events as unknown as RIEvent[]);
+			const groupedEvents = groupOverlappingEvents(events as unknown as IEvent_V2[]);
 			expect(groupedEvents).toEqual([
 				[events[0]],
 				[events[1]],
@@ -57,7 +57,7 @@ describe('Event Utils', () => {
 			{id: 2.0, start: '2022-01-01T12:00:00', end: '2022-01-01T13:00:00'},
 			{id: 2.1, start: '2022-01-01T12:59:00', end: '2022-01-01T15:00:00'},
 		];
-		const groupedEvents = groupOverlappingEvents(events as unknown as RIEvent[]);
+		const groupedEvents = groupOverlappingEvents(events as unknown as IEvent_V2[]);
 		expect(groupedEvents).toEqual([
 			[events[0], events[1], events[2], events[3]],
 			[events[4], events[5]],
