3eceb8908daca192cd68db91345796517743e032
Karba <ivan.karbashevskyi@gmail.com>
2024-06-20 12:32:25 +0200

    Add local production config

--- src/environment/environment.local_production.ts
+++ src/environment/environment.local_production.ts
@@ -1,14 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The settings of file replacements can be found in `angular.json`.
+
 import {constant} from "@environment/constant";
 
 export const environment = {
-	production: true,
-	develop: false,
+	production: false,
+	develop: true,
 	emulator: false,
 	proxy: false,
 	setDefaultValueToInputs: false,
 	apiUrls: {
-		panel: 'https://api.beeoclock.com/panel',
-		identity: 'https://api.beeoclock.com/identity',
+		panel: 'local.panel',
+		identity: 'local.identity',
 	},
 	endpoint: {
 		config: {
@@ -47,3 +51,12 @@ export const environment = {
 		publicPageOrigin: 'https://beeoclock.com',
 	}
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
