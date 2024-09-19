540c85f5ab4f49755200c22d32c1ab9cb5b36d85
Karba <ivan.karbashevskyi@gmail.com>
2024-09-19 13:21:10 +0200

    Chore with typia

--- src/module/customer/domain/interface/i.customer.ts
+++ src/module/customer/domain/interface/i.customer.ts
@@ -1,21 +1,22 @@
 import {Enum, IBaseEntity} from "@src/module/utility/domain";
-import typia, {tags} from "typia";
 import {CustomerTypeEnum} from "@customer/domain/enum/customer-type.enum";
+import {Tools} from "@utility/tools";
+import {Types} from "@utility/types";
 
 /**
  * Declare interface by business logic, if you need case when each property is optional, use Partial<ICustomer>
  */
 export interface ICustomer extends IBaseEntity<'CustomerDto'> {
-	firstName: string & tags.MaxLength<50> | null;
-	lastName: string & tags.MaxLength<50> | null;
+	firstName: string & Types.MaxLength<50> | null;
+	lastName: string & Types.MaxLength<50> | null;
 	phone: string | null;
-	email: string & tags.Format<"email"> | null;
+	email: string & Types.Format<"email"> | null;
 	note: string | null;
-	customerType: CustomerTypeEnum & tags.Default<CustomerTypeEnum.new>;
+	customerType: CustomerTypeEnum & Types.Default<CustomerTypeEnum.new>;
 
 	active: Enum.ActiveEnum;
 }
 
-export const isCustomer = typia.createIs<ICustomer>();
-export const validCustomer = typia.createValidate<ICustomer>();
-export const randomCustomer = typia.createRandom<ICustomer>();
+export const isCustomer = Tools.createIs<ICustomer>();
+export const validCustomer = Tools.createValidate<ICustomer>();
+export const randomCustomer = Tools.createRandom<ICustomer>();
---
+++ src/module/utility/types.ts
@@ -0,0 +1,14 @@
+import {tags} from 'typia';
+import {Format} from "typia/src/tags/Format";
+
+export namespace Types {
+	export type Format<Value extends keyof Format.Validator> = tags.Format<Value>;
+	export type Default<Value extends boolean | bigint | number | string> = tags.Default<Value>;
+	export type Minimum<Value extends number> = tags.Minimum<Value>;
+	export type Maximum<Value extends number> = tags.Maximum<Value>;
+	export type MaxLength<Value extends number> = tags.MaxLength<Value>;
+	export type Pattern<Value extends string> = tags.Pattern<Value>;
+	export type ObjectId = Pattern<"^[0-9a-fA-F]{24}$">;
+	export type DateTime = Format<"date-time">;
+	export type SID = Pattern<"^SM[0-9a-fA-F]{32}$">;
+}
