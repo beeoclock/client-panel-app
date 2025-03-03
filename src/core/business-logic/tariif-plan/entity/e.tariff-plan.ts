98c03c20f91fa04ce3e25e7cd76424dfb12e7239
Karba <ivan.karbashevskyi@gmail.com>
2025-03-03 09:33:58 +0100

    (core): Add new core "tariff-plan"

---
+++ src/core/business-logic/tariif-plan/entity/e.tariff-plan.ts
@@ -0,0 +1,64 @@
+import {ABaseEntity} from "@core/system/abstract/a.base-entity";
+import {ActiveEnum} from "@core/shared/enum";
+import {ITariffPlan} from "@core/business-logic/tariif-plan/interface/i.tariff-plan";
+import {BillingCycleEnum} from "@core/shared/enum/billing-cycle.enum";
+import {TypeTariffPlanEnum} from "../enum/type.tariff-plan.enum";
+
+export class ETariffPlan extends ABaseEntity<'TariffPlanDto', ITariffPlan.DTO, ITariffPlan.EntityRaw> implements ITariffPlan.EntityRaw {
+
+	override object = 'TariffPlanDto' as const;
+
+	type!: TypeTariffPlanEnum;
+	prices!: ITariffPlan.IPrice[];
+	isPerSpecialist!: boolean;
+	billingCycle!: BillingCycleEnum;
+	specialistLimit!: number;
+	features!: string[];
+	active!: ActiveEnum;
+	pluginAttachment!: ITariffPlan.IPluginAttachment;
+
+
+	public override toDTO(): ITariffPlan.DTO {
+		return ETariffPlan.toDTO(this);
+	}
+
+	public static toDTO(data: ITariffPlan.EntityRaw): ITariffPlan.DTO {
+		return {
+			object: data.object,
+			_id: data._id,
+
+			active: data.active,
+			billingCycle: data.billingCycle,
+			features: data.features,
+			isPerSpecialist: data.isPerSpecialist,
+			pluginAttachment: data.pluginAttachment,
+			specialistLimit: data.specialistLimit,
+			prices: data.prices,
+			type: data.type,
+
+			state: data.state,
+			stateHistory: data.stateHistory,
+			createdAt: data.createdAt,
+			updatedAt: data.updatedAt,
+		}
+	}
+
+	/**
+	 * Use it to create new entity, e.g. from API or form
+	 * @param data
+	 */
+	public static fromDTO(data: ITariffPlan.DTO): ETariffPlan {
+		return new ETariffPlan(data);
+	}
+
+	/**
+	 * Use it to create entity from raw data, e.g. from database
+	 * @param data
+	 */
+	public static fromRaw(data: ITariffPlan.EntityRaw): ETariffPlan {
+		return new ETariffPlan(data);
+	}
+
+}
+
+export default ETariffPlan;
---
+++ src/core/business-logic/tariif-plan/interface/i.tariff-plan.ts
@@ -0,0 +1,52 @@
+import {IBaseDTO, IBaseEntityRaw} from "@utility/domain";
+import {Tools} from "@core/shared/tools";
+import {ActiveEnum, CurrencyCodeEnum, LanguageCodeEnum} from "@core/shared/enum";
+import {CountryCodeEnum} from "@core/shared/enum/country-code.enum";
+import {RegionCodeEnum} from "@core/shared/enum/region-code.enum";
+import {TypeTariffPlanEnum} from "@core/business-logic/tariif-plan/enum/type.tariff-plan.enum";
+import {BillingCycleEnum} from "@core/shared/enum/billing-cycle.enum";
+
+
+export namespace ITariffPlan {
+
+	export interface ILanguageVersion {
+		object: "LanguageVersionDto";
+		title: string;
+		description: string;
+		language: LanguageCodeEnum;
+	}
+
+	export interface IPrice {
+		country: CountryCodeEnum;
+		region: RegionCodeEnum;
+		value: number;
+		currency: CurrencyCodeEnum;
+		languageVersions: ILanguageVersion[]
+	}
+
+	export interface IPluginAttachment {
+		includeAll: boolean;
+		excludeAll: boolean;
+		includeList: string[];
+		excludeList: string[];
+	}
+
+	export interface DTO extends IBaseDTO<'TariffPlanDto'> {
+		type: TypeTariffPlanEnum;
+		prices: IPrice[];
+		isPerSpecialist: boolean;
+		billingCycle: BillingCycleEnum;
+		specialistLimit: number;
+		features: string[];
+		active: ActiveEnum;
+		pluginAttachment: IPluginAttachment;
+	}
+
+	export type EntityRaw = IBaseEntityRaw<'TariffPlanDto'> & DTO & {};
+
+}
+
+
+export const isTariffPlan = Tools.createIs<ITariffPlan.DTO>();
+export const validTariffPlan = Tools.createValidate<ITariffPlan.DTO>();
+export const randomTariffPlan = Tools.createRandom<ITariffPlan.DTO>();
