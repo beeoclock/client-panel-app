4c2ef7f8986c7af37e2eb8fd1016ccf4fd1c1cb8
Karba <ivan.karbashevskyi@gmail.com>
2025-03-04 17:20:58 +0100

    Fix

--- src/[tenant]/module/tariff-plan/infrastructure/api/patch/patch.tenant-tariff-plan.change.api.ts
+++ src/[tenant]/module/tariff-plan/infrastructure/api/patch/patch.tenant-tariff-plan.change.api.ts
@@ -3,15 +3,17 @@ import {BaseApiAdapter} from "@core/shared/adapter/base.api.adapter";
 import {ITariffPlan} from "@core/business-logic/tariif-plan/interface/i.tariff-plan";
 import {TariffPlanEndpointEnum} from "@tariffPlan/infrastructure/endpoint/tariff-plan.endpoint";
 
+type Response = {url: string;};
+
 @Injectable()
-export class PatchTenantTariffPlanChangeApi extends BaseApiAdapter<string, [ITariffPlan.DTO]> {
+export class PatchTenantTariffPlanChangeApi extends BaseApiAdapter<Response, [ITariffPlan.DTO]> {
 
 	/**
 	 * @param body
 	 */
 	public override execute$(body: ITariffPlan.DTO) {
 
-		return this.httpClient.patch<string>(TariffPlanEndpointEnum.PATCH__TENANT_TARIFF_PLAN__CHANGE, body);
+		return this.httpClient.patch<Response>(TariffPlanEndpointEnum.PATCH__TENANT_TARIFF_PLAN__CHANGE, body);
 	}
 
 }
