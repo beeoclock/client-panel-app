b54fb92c8233225e28b674642db5ab37e7935233
Karba <ivan.karbashevskyi@gmail.com>
2025-03-25 22:33:47 +0100

    fix

--- src/[tenant]/payment/infrastructure/data-provider/indexedDB/adapter/payment.dexie.adapter.indexedDB.data-provider.ts
+++ src/[tenant]/payment/infrastructure/data-provider/indexedDB/adapter/payment.dexie.adapter.indexedDB.data-provider.ts
@@ -7,7 +7,7 @@ import {IPayment} from "@core/business-logic/payment/interface/i.payment";
 @Injectable()
 export class PaymentDexieAdapterIndexedDBDataProvider extends DexieAdapterIndexedDBDataProvider<IPayment.EntityRaw> {
 
-	protected readonly columns = '_id,state,createdAt,updatedAt';
+	protected readonly columns = '_id,state,createdAt,updatedAt,orderId,anchorId';
 	protected readonly moduleName = 'payment';
 	protected readonly version = 1;
 
